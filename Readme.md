8ec544d35cebbc6ad75434e20d7cc13f9d41dff6
Abensett <bensettiaimen@mailo.com>
2025-03-07 19:02:22 +0100

    First Push

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Activité 3 Pendu
+
+## Programmation Orientée Objet <=> POO
+
+### Class vs Instance
+* Class <-> On crée notre propre type
+* Instance <-> Une version de cette classe
+
+Exemple : Class d'une voiture, on définit tous ce que contient la classe : ATTRIBUTS & METHODES
+
+* Attributs = Vatiable dans la classes <=> Poids (int), Couleur (str)
+* Methodes = Rouler(), Freiner()
+
+
+# Git
+* Pour télécharger un répo : git clone [URL] ([Nom_dossier_sur_l'ordi])
+* Pour récupérer les modifications d'un projet : git pull
--- main.py
+++ main.py
@@ -2,62 +2,38 @@
 from flask import Flask, render_template, session, redirect
 # Importation du module os
 import os
-# Importation de nos questions
-from questions import questions
-# Importation des resultats
-from questions import resultats
+# Importation du module os
+import random
 
 
 # Création de l'instance de l'app Flask
-app = Flask("Ma_webapp")
+app = Flask("Jeu du Pendu")
 # Création d'une clef secrete
 app.secret_key = os.urandom(24)
 
-# Route principal "/"-> notre page d'accueil qui est donc à la racine du site
+# Premier route -> première page affichée lorsqu'on arrive sur l'application web 
+# Rénitialiser le jeu
 @app.route("/")
-def index():
-    session["numero_question"] = 0
-    session["score"] = {"Pikachu": 0, "Mew" : 0 , "Salamèche": 0, "Carapuce" :0}
+def accueil():
+    # Liste de mots possibles
+    liste_de_mots = ["ordinateur", "clavier", "magic", "araignée", "souris", "fenêtre", "papier", "arbre", "fleur"]
+    # On choisit un mot au hasard
+    mot_a_deviner = random.choice(liste_de_mots)
 
-    return render_template("index.html")
+    print(mot_a_deviner)
+    return "Test"
 
-# Route pour les différentes questions
-@app.route("/question")
-def question():
-    # On accède à la variable globale question
-    global questions
-    nb_question = session["numero_question"] 
-    # Si on ne dépasse pas le nombre de question
-    if nb_question < len(questions) :
-        # On récupère l'énoncé de la question
-        enonce_question = questions[nb_question]["enonce"]
-        # On retire la question
-        questions_copy = questions[nb_question].copy()
-        questions_copy.pop("enonce")
-        # On récupère les réponses sous forme de liste
-        reponses = list(questions_copy.values())
-        # On récupère les clefs pour le comptage du score
-        session["clefs"] = list(questions_copy.keys())
 
-        return render_template("question.html", question = enonce_question, reponses = reponses)
-    else :
-        global resultats
-        # On trie les scores dans l'odre décroissant
-        score = sorted(session["score"], key=session["score"].get , reverse = True)
-        # On stocke le nom du vainqueur -> Premier de la liste score
-        vainqueur = score[0]
-        # On récupère la description associé au vainqueur
-        description = resultats[vainqueur]
-        return render_template("resultat.html", vainqueur = vainqueur, description = description)
 
-# Route pour la sélection d'une réponse puis redirection vers la prochaine question
-@app.route("/reponse/<numero>")
-def reponse(numero):
-    session["numero_question"] += 1
-    resultat = session["clefs"][int(numero)]
-    session["score"][resultat] += 1
-    print(session["score"])
-    return redirect("/question")
+
+
+
+
+
+
+
+
+
 
 
 # Exécution
